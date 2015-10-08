6b14648fce3daddd01ee3c4c8a8100463f31ad4d
王长慧 <Tarenas@cengziqiang.local>
2015-10-08 12:33:38 +0800

    hehe

---
+++ demo2/demo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo2
+//
+//  Created by 王长慧 on 15/10/8.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo2/demo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo2
+//
+//  Created by 王长慧 on 15/10/8.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
